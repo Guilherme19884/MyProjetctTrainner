de5a4ebcc689b8afdad7940f7d9ac89bde78abcc
Guilherme <guilherme1984.araujo@gmail.com>
2024-07-18 09:50:43 -0300

    instalado o .env e configurado a porta

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,11 @@ import  express from 'express'
 import cors from 'cors'
 import { AppDataSource } from './database/data-source'
 import routers from './app/routes/routes'
+import dotenv from 'dotenv'
 
-const app = express();
+dotenv.config()
+const port = process.env.PORT || 3000
+const app = express()
 
 app.use(cors())
 app.use(express.json())
@@ -13,8 +16,8 @@ app.use('/', routers)
 
 AppDataSource.initialize().then(async() =>{
     console.log('Database Ok! 🎲')
-    app.listen(3000, ()=> {
-        console.log('Servidor rodando na porta 3000.')
+    app.listen(port, ()=> {
+        console.log(`Servidor rodando na porta ${port}`)
     })
 })
 
