e4c4a0be9dc32740bbf3b9bd7a7ee0911314e57a
Guilherme <guilherme1984.araujo@gmail.com>
2024-07-19 16:03:56 -0300

    criado método chooseModality

--- src/app/entities/Evaluation.ts
+++ src/app/entities/Evaluation.ts
@@ -1,9 +1,9 @@
 import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm"
-import Modalidades from "./Modalidades";
-import User from "./User";
+import Modalidades from "./Modalidades"
+import User from "./User"
 
 @Entity('evaluations')
-export class Evaluation{
+class Evaluation {
 
     @PrimaryGeneratedColumn('increment')
     private id: number = 0
@@ -14,10 +14,33 @@ export class Evaluation{
     @Column('varchar', { length: 100, nullable:false })
     private locationOfTest: string = ''
 
+    @Column('int', { nullable:false })
+    private distanceOfTestInMeters: number = 0
+
     @Column('enum', { enum: Modalidades, nullable: false })
     modalidade: Modalidades = Modalidades.CORRIDA || Modalidades.CICLISMO || Modalidades.DUATHLON
 
     @ManyToOne(() => User, user => user.evaluations)
     @JoinColumn({ name: 'user_id' })
     user: User | undefined
+
+    //Escolher modalidade
+    public ChooseModality (modalidade: Modalidades): void {
+        this.modalidade = modalidade
+    }
+
+    //Realizar avaliação de corrida (Teste de Cooper) Importante enfatizar o tempo de 12 minutos, local plano.
+    public calculateVo2Max(): void {
+        const Vo2max = this.distanceOfTestInMeters - 504.9 / 44.73
+    }
+
+    //Estimativa de Ritmo de corrida baseado no vo2máx
+
+    //Estimativa de corrida a 80% do vo2máx
+
+
+    //Realizar avaliação de ciclismo
+
 }
+
+export default Evaluation
